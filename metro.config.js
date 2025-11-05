d621680117a922fd6591c1f28cf7fa66a728b7d1
natively.dev <api@natively.dev>
2025-11-05 15:10:04 +0000

    fix: Enable unstable_enablePackageExports for Expo SDK 53

--- metro.config.js
+++ metro.config.js
@@ -1,12 +1,16 @@
+
 const { getDefaultConfig } = require('expo/metro-config');
 const { FileStore } = require('metro-cache');
 const path = require('path');
 
 const config = getDefaultConfig(__dirname);
 
+// Enable package exports for Expo SDK 53
+config.resolver.unstable_enablePackageExports = true;
+
 // Use turborepo to restore the cache when possible
 config.cacheStores = [
-    new FileStore({ root: path.join(__dirname, 'node_modules', '.cache', 'metro') }),
-  ];
+  new FileStore({ root: path.join(__dirname, 'node_modules', '.cache', 'metro') }),
+];
 
 module.exports = config;
